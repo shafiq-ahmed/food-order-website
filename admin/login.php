97d23b8e942d1d1b75b8532f69f8b51f288ecaf4
Shafiq Ahmed <shafiqahmd3@gmail.com>
2023-03-14 16:32:53 +0600

    login and logout feature added

---
+++ admin/login.php
@@ -0,0 +1,50 @@
+<?php include "../config/constants.php";?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Login - Food Order
+        </title>
+        
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <div class="login">
+            <h1 class="text-center">Login</h1><br>
+            <form action="" method="post" class="text-center">
+                Username<br>
+                <input type="text" name="username"><br><br>
+                Password<br>
+                <input type="password" name="password"><br><br>
+                <input type="submit" name="login" value="Login" class="btn-secondary">
+            </form>
+             
+        </div>
+    </body>
+</html>
+<?php
+    
+    if(isset($_POST['login']))
+    {
+        echo $username=$_POST['username'];
+        echo $password=$_POST['password'];
+
+        $sql="SELECT * FROM admin WHERE username='$username' AND password='$password'";
+
+        $res=mysqli_query($con,$sql) or die(mysqli_error($con));
+
+        if(mysqli_num_rows($res)==1)
+        {
+            /*$row=mysqli_fetch_assoc($res);
+            echo $row['id']."<br>";
+            echo $row['username']."<br>";
+            echo $row['password']."<br>";*/
+
+            header('location:'.SITEURL.'admin/index.php');
+        }else
+        {
+            echo "User does not exist";
+        }
+    }
+?>  
