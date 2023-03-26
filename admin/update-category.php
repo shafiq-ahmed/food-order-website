2e2c02ad0d190c752d8a421604805bfe944fdef4
Shafiq Ahmed <shafiqahmd3@gmail.com>
2023-03-26 08:30:57 +0600

    Update update-category.php

--- admin/update-category.php
+++ admin/update-category.php
@@ -8,17 +8,38 @@
         ?>
         <form action="" method="post">
             <table class="tbl-30">
+                <?php
+                    $sql="SELECT * FROM category WHERE id='$id'";
+
+                    $res=mysqli_query($con,$sql);
+
+                    if(mysqli_num_rows($res)==1)
+                    {
+                        $row=mysqli_fetch_assoc($res);
+                        $title=$row['title'];
+                        $featured=$row['featured'];
+                        $active=$row['active'];
+                        $imageName=$row['image_name'];
+                    }
+                ?>
                 <tr>
                     <td>Title</td>
                     <td>:</td>
-                    <td><input type="text" name="title"></td>
+                    <td><input type="text" name="title" placeholder="<?php echo $title;?>" value="<?php echo $title;?>"></td>
+                </tr>
+
+                <tr>
+                    <td>Image</td>
+                    <td>:</td>
+                    <td><img src="<?php echo CATEGORY_IMAGE_SOURCE.$imageName?>" width='100px'></td><br>
+                    <td><input type="file" name="image"></td>
                 </tr>
 
                 <tr>
                     <td>Featured</td>
                     <td>:</td>
-                    <td><input type="radio" name="featured" value="Yes" >Yes
-                        <input type="radio" name="featured" value="No" checked>No
+                    <td><input type="radio" name="featured" value="Yes" <?php echo ($featured=="Yes")?"Checked":""?> >Yes
+                        <input type="radio" name="featured" value="No" <?php echo ($featured=="No")?"Checked":""?>>No
                     
                     </td>
                 </tr>
@@ -26,8 +47,8 @@
                 <tr>
                     <td>Active</td>
                     <td>:</td>
-                    <td><input type="radio" name="active" value="Yes" >Yes
-                        <input type="radio" name="active" value="No" checked>No
+                    <td><input type="radio" name="active" value="Yes" <?php echo ($featured=="Yes")?"Checked":""?> >Yes
+                        <input type="radio" name="active" value="No" <?php echo ($featured=="No")?"Checked":""?>>No
                     
                     </td>
                 </tr>
